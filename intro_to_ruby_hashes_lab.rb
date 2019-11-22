099921742f3d4ab212acee404a435a455182ac2a
Erik Kasperson <chefkasperson@gmail.com>
2019-11-22 20:02:32 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -48,7 +48,7 @@ def monopoly_with_fourth_tier
 	    :pieces => 4,
 	    :names => {
 	      :reading_railroad => {
-	        "Mortgage Value" => ("$" + 100)
+	        "Mortgage Value" => "$100"
 	      },
 	      :pennsylvania_railroad => {},
 	      :b_and_o_railroad => {},
