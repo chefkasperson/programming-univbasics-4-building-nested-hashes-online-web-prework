8533f39f6e2c20806925347f4802af8dcd7c7d37
Erik Kasperson <chefkasperson@gmail.com>
2019-11-22 19:55:14 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -23,10 +23,10 @@ def monopoly_with_third_tier
 	  :railroads => {
 	    :pieces => 4,
 	    :names => {
-	      :one => "Blah",
-	      :two => "Glah",
-	      :three => "Gee",
-	      :four => "Gosh"
+	      :reading_railroad => {},
+	      :two => {},
+	      :three => {},
+        :four => {}
 	    },
 	    :rent_in_dollars => {
 	      :one_piece_owned => 25,
